74ff6ddf2f3896b0af96887889b5a58b4431d46b
Johannès Chapas <johannes.chapas@gmail.com>
2017-01-21 21:39:15 +0100

    Feat exemple de repère, popup, événement & trajet

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//Initialisation de la carte, centrage sur l'Australie
 var map = L.map('mapid').setView([-38.746,148.228], 6);
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+//Téléchargement du fond de carte chez OSM
 var osm = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
+//Affichage du tracé GPX d'exemple grâce à leaflet-omnivore
+omnivore.gpx('trajet.gpx').addTo(map);
+//Ajout d'un repère et d'une popup qui s'ouvre toute seule
+var repere1 = L.marker([-38.746,148.228]).addTo(map)
+  .bindPopup('<h1>Titre de la popup</h1> Suite de la popup')
+  .openPopup();
+//Association d'un évènement au repère
+repere1.on('click', function(e) {
+  //Code à executer lors du clic sur le repère
+  alert("Repère cliqué");
+});

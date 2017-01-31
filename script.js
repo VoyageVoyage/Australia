d5010d53b50f155ae0ad20fc48d10321e9baac3f
Bichonus <clementine.decarlan@etu.emse.fr>
2017-01-31 12:33:08 +0100

    Feat ajout chemin

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var map = L.map('mapid').setView([-38.746,148.228], 6);
 //Téléchargement du fond de carte chez OSM
 var osm = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
 //Affichage du tracé GPX d'exemple grâce à leaflet-omnivore
-omnivore.gpx('trajet.gpx').addTo(map);
+omnivore.gpx('chemin.gpx').addTo(map);
 //Ajout d'un repère et d'une popup qui s'ouvre toute seule
 var repere1 = L.marker([-38.746,148.228]).addTo(map)
 
